ae41a07d9f82d82df719334feed0c83d98f29e69
Francisco Borquez <fborquez@outlook.com>
2022-08-15 23:17:45 -0400

    fix: how to call pokemon command

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { init } from 'cordless';
-import { pokemon } from '@functions/pokemon';
+import { command } from '@functions/pokemon';
 import { BotOptions } from 'types/botOptions';
 import { config } from '@helpers/config';
 
 const botToken: string = config('DISCORD_BOT_TOKEN');
 const options: BotOptions = {
-  commands: [pokemon], 
+  commands: [command], 
   token: botToken,
 }
 
